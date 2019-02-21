7ada4f6341665d0faa554f6e6c809174b15c3e5a
shakogele <shakogele@dspro.io>
2019-02-21 18:50:16 +0400

    Chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,20 +9,32 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-  let arr = [];
   let bigArr = [];
-
-  for(let single of array){
-    arr.push(single);
-    if(single % size === 0){
-      bigArr.push(arr);
-      arr = [];
+  let slicer = 0;
+  for(let ind in array){
+    if( (parseInt(ind) === 0) || (parseInt(ind) % size === 0)){
+      let newArr = array.slice(parseInt(ind), parseInt(ind) + size);
+      bigArr.push(newArr);
     }
-  }
-  if(arr.length < size && arr.length > 0){
-    bigArr.push(arr)
   }
   return bigArr;
 }
 
 module.exports = chunk;
+// First Solution
+// function chunk(array, size) {
+//   let arr = [];
+//   let bigArr = [];
+//
+//   for(let single of array){
+//     arr.push(single);
+//     if(single % size === 0){
+//       bigArr.push(arr);
+//       arr = [];
+//     }
+//   }
+//   if(arr.length < size && arr.length > 0){
+//     bigArr.push(arr)
+//   }
+//   return bigArr;
+// }
